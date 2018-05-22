077b93f1476e63bf564a864486b3142547f9b65c
Andrey Orst <andreyorst@gmail.com>
2018-05-22 11:33:49 +0300

    typo

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,7 +3,7 @@ ERROR=0
 echo "Starting tmux session"
 tmux new-session -d -n SimpleSnippetsTest
 
-echo "Running tests"
+echo "Running tests:"
 tests/for_test/test.sh || ERROR=$[ $ERROR + 1 ]
 tests/cla_test/test.sh || ERROR=$[ $ERROR + 1 ]
 
