2e6f73fe8317d2c6907366cf5497aad9bdc00c55
Andrey Orst <andreyorst@gmail.com>
2018-05-22 11:43:42 +0300

    check if travis can handle file creation

--- tests/cla_test/test.sh
+++ tests/cla_test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $(pwd)
+touch tests/cla_test/cla_test_result.cpp
 tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla.cpp" enter "i/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-j "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x! tests/cla_test/cla_test_result.cpp" enter
 
 SHA_REF=$(sha256sum tests/cla_test/cla_test_reference.cpp | sed -E "s/(\w+).*/\1/")
