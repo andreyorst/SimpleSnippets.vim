5d24c6b86cb302b1ad702fcbaace4d14fd8b4ce4
Andrey Orst <andreyorst@gmail.com>
2018-05-22 11:41:02 +0300

    add missing reference for cla test

---
+++ tests/cla_test/cla_test_reference.cpp
@@ -0,0 +1,15 @@
+/* test start */
+#ifndef SIMPLE_SNIPPETS_H
+#define SIMPLE_SNIPPETS_H
+
+class SimpleSnippets {
+	public:
+		SimpleSnippets();
+		virtual ~SimpleSnippets();
+
+	private:
+		char simple
+};
+
+#endif /* SIMPLE_SNIPPETS_H */
+/* test end */
--- tests/cla_test/test.sh
+++ tests/cla_test/test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla.cpp" enter "i/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-j "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x! tests/cla_test/cla_test_result.c" enter
+echo $(pwd)
+tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla.cpp" enter "i/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-j "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x! tests/cla_test/cla_test_result.cpp" enter
 
-SHA_REF=$(sha256sum tests/cla_test/cla_test_reference.c | sed -E "s/(\w+).*/\1/")
-SHE_RES=$(sha256sum tests/cla_test/cla_test_result.c | sed -E "s/(\w+).*/\1/")
+SHA_REF=$(sha256sum tests/cla_test/cla_test_reference.cpp | sed -E "s/(\w+).*/\1/")
+SHE_RES=$(sha256sum tests/cla_test/cla_test_result.cpp | sed -E "s/(\w+).*/\1/")
 
 if [[ $SHA_REF != $SHA_RES ]]; then
     echo "[ERR]: cla test"
