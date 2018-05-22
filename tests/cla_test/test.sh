9858eefffa8adbc45137ca308035fcf9a4bdbbc5
Andrey Orst <andreyorst@gmail.com>
2018-05-22 14:14:43 +0300

    reworked tests

--- tests/cla_test/test.sh
+++ tests/cla_test/test.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla_test_result.cpp" enter "ggdGi/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-k "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x! tests/cla_test/cla_test_result.cpp" enter
+ERROR=0
+cd cla_test/
 
-SHA_REF=$(sha256sum tests/cla_test/cla_test_reference.cpp | sed -E "s/(\w+).*/\1/")
-SHA_RES=$(sha256sum tests/cla_test/cla_test_result.cpp | sed -E "s/(\w+).*/\1/")
+tmux new-session -d -n SimpleSnippetsTest
+tmux send-keys -t SimpleSnippetsTest "nvim -u ../testrc cla_test_result.cpp" enter "ggdGi/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-k "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x" enter
+
+sleep 1
+
+SHA_REF=$(sha256sum cla_test_reference.cpp | sed -E "s/(\w+).*/\1/")
+SHA_RES=$(sha256sum cla_test_result.cpp | sed -E "s/(\w+).*/\1/")
 
 if [[ $SHA_REF != $SHA_RES ]]; then
     echo "[ERR]: cla test"
-    exit 1
+    mv cla_test_result.cpp log
+    ERROR=1
 else
     echo "[OK]: cla test"
-    exit 0
+    rm cla_test_result.cpp
 fi
+
+tmux kill-window -t SimpleSnippetsTest
+cd ..
+exit $ERROR
