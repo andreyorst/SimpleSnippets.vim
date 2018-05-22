a1b9db13b99fbea0073286546a98dcb402f10cb5
Andrey Orst <andreyorst@gmail.com>
2018-05-22 23:57:50 +0300

    added array of tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,15 +2,16 @@
 error=0
 verbose=1
 vim_versions=("nvim" "vim")
+tests=("lorem_test" "for_test" "cla_test")
 
 cd tests
 for vim in ${vim_versions[*]}; do
     echo -n "Running tests for $vim:"
     [[ $verbose != 0 ]] && echo
 
-    lorem_test/test.sh $vim $verbose || error=$[ $error + 1 ]
-    for_test/test.sh $vim $verbose || error=$[ $error + 1 ]
-    cla_test/test.sh $vim $verbose || error=$[ $error + 1 ]
+    for test in ${tests[*]}; do
+        $test/test.sh $vim $verbose || error=$[ $error + 1 ]
+    done
 
     if [[ $verbose == 0 ]]; then
         if [[ $error == 0 ]]; then
