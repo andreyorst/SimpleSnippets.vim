92c5d599e43016a574c25a7bbf919a325d6bbcad
Andrey Orst <andreyorst@gmail.com>
2018-05-22 11:31:10 +0300

    extended if

--- tests/for_test/test.sh
+++ tests/for_test/test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/for_test/for.c" enter "i/* test start */" enter "for" escape "a" tab "22" tab "j" tab "10" c-k c-k "0" tab "l" tab "100" tab ">" tab "--" c-j "while body" c-k "char" tab "// for body" tab enter "/* test end */" escape ":x! tests/for_test/for_test_result.c" enter
-[[ $(sha256sum tests/for_test/for_test_reference.c | sed -E "s/(\w+).*/\1/") == $(sha256sum tests/for_test/for_test_result.c | sed -E "s/(\w+).*/\1/") ]]
+
+SHA_REF=$(sha256sum tests/for_test/for_test_reference.c | sed -E "s/(\w+).*/\1/")
+SHE_RES=$(sha256sum tests/for_test/for_test_result.c | sed -E "s/(\w+).*/\1/")
+
+if [[ $SHA_REF != $SHA_RES ]]; then
+    echo "[ERR]: for test"
+    exit 1
+else
+    exit 0
+fi
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ERROR=0
+echo "Starting tmux session"
 tmux new-session -d -n SimpleSnippetsTest
+
+echo "Running tests"
 tests/for_test/test.sh || ERROR=$[ $ERROR + 1 ]
+tests/cla_test/test.sh || ERROR=$[ $ERROR + 1 ]
+
 pkill tmux
 exit $ERROR
