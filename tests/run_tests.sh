cdd7ec65920c7582ae7fab1872d0a6644bf8b820
Andrey Orst <andreyorst@gmail.com>
2018-05-22 17:20:57 +0300

    running tests for several vim versions

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ERROR=0
 cd tests
-echo "Running tests:"
-vim=nvim
-for_test/test.sh $vim || ERROR=$[ $ERROR + 1 ]
-cla_test/test.sh $vim || ERROR=$[ $ERROR + 1 ]
+vim_versions=("nvim" "vim")
+for vim in ${vim_versions[*]}; do
+    echo "Running tests for: $vim"
+    for_test/test.sh $vim || ERROR=$[ $ERROR + 1 ]
+    cla_test/test.sh $vim || ERROR=$[ $ERROR + 1 ]
+done
 exit $ERROR
