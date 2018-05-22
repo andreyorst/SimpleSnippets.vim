90df7f4eb00819efc6ba575a26ebdffef6c3b229
Andrey Orst <andreyorst@gmail.com>
2018-05-22 11:47:12 +0300

    trying to manually add files

--- tests/cla_test/test.sh
+++ tests/cla_test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 touch tests/cla_test/cla_test_result.cpp
-tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla.cpp" enter "i/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-j "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x! tests/cla_test/cla_test_result.cpp" enter
+tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla_test_result.cpp" enter "i/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-j "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":w tests/cla_test/cla_test_result.cpp" enter
 
 SHA_REF=$(sha256sum tests/cla_test/cla_test_reference.cpp | sed -E "s/(\w+).*/\1/")
 SHE_RES=$(sha256sum tests/cla_test/cla_test_result.cpp | sed -E "s/(\w+).*/\1/")
--- tests/for_test/test.sh
+++ tests/for_test/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/for_test/for.c" enter "i/* test start */" enter "for" escape "a" tab "22" tab "j" tab "10" c-k c-k "0" tab "l" tab "100" tab ">" tab "--" c-j "while body" c-k "char" tab "// for body" tab enter "/* test end */" escape ":x! tests/for_test/for_test_result.c" enter
+touch tests/for_test/for_test_result.c
+tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/for_test/for_test_result.c" enter "i/* test start */" enter "for" escape "a" tab "22" tab "j" tab "10" c-k c-k "0" tab "l" tab "100" tab ">" tab "--" c-j "while body" c-k "char" tab "// for body" tab enter "/* test end */" escape ":w tests/for_test/for_test_result.c" enter
 
 SHA_REF=$(sha256sum tests/for_test/for_test_reference.c | sed -E "s/(\w+).*/\1/")
 SHE_RES=$(sha256sum tests/for_test/for_test_result.c | sed -E "s/(\w+).*/\1/")
