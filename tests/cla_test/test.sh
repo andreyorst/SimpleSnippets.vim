5083e8f898f0f31886bfdd8a22df174efe1c08db
Andrey Orst <andreyorst@gmail.com>
2018-05-22 12:24:05 +0300

    fix mapping error in cla

--- tests/cla_test/test.sh
+++ tests/cla_test/test.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla_test_result.cpp" enter "i/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-j "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape "x! tests/cla_test/cla_test_result.cpp" enter
+tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/cla_test/cla_test_result.cpp" enter "ggdGi/* test start */" enter "cla" escape "a" tab "travis" tab "TRAVIS_H" tab "int trav" c-k c-k "SimpleSnippets" c-j "char simple" c-k "SIMPLE_SNIPPETS_H" tab tab enter "/* test end */" escape ":x! tests/cla_test/cla_test_result.cpp" enter
 
 SHA_REF=$(sha256sum tests/cla_test/cla_test_reference.cpp | sed -E "s/(\w+).*/\1/")
 SHA_RES=$(sha256sum tests/cla_test/cla_test_result.cpp | sed -E "s/(\w+).*/\1/")
 
 if [[ $SHA_REF != $SHA_RES ]]; then
     echo "[ERR]: cla test"
-    rm tests/cla_test/cla_test_result.cpp
     exit 1
 else
     echo "[OK]: cla test"
-    rm tests/cla_test/cla_test_result.cpp
     exit 0
 fi
--- tests/for_test/test.sh
+++ tests/for_test/test.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/for_test/for_test_result.c" enter "i/* test start */" enter "for" escape "a" tab "22" tab "j" tab "10" c-k c-k "0" tab "l" tab "100" tab ">" tab "--" c-j "while body" c-k "char" tab "// for body" tab enter "/* test end */" escape ":x! tests/for_test/for_test_result.c" enter
+tmux send-keys -t SimpleSnippetsTest "nvim -u tests/testrc tests/for_test/for_test_result.c" enter "ggdGi/* test start */" enter "for" escape "a" tab "22" tab "j" tab "10" c-k c-k "0" tab "l" tab "100" tab ">" tab "--" c-j "while body" c-k "char" tab "// for body" tab enter "/* test end */" escape ":x! tests/for_test/for_test_result.c" enter
 
 SHA_REF=$(sha256sum tests/for_test/for_test_reference.c | sed -E "s/(\w+).*/\1/")
 SHA_RES=$(sha256sum tests/for_test/for_test_result.c | sed -E "s/(\w+).*/\1/")
 
 if [[ $SHA_REF != $SHA_RES ]]; then
     echo "[ERR]: for test"
-    rm tests/for_test/for_test_result.c
     exit 1
 else
     echo "[OK]: for test"
-    rm tests/for_test/for_test_result.c
     exit 0
 fi
 
