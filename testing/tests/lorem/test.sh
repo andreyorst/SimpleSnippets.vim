42e06f4b128c99627ed1fb4581b8b1e1c7f3fbbb
Andrey Orst <andreyorst@gmail.com>
2018-05-24 16:17:03 +0300

    fix calling for vim twice

--- testing/tests/lorem/test.sh
+++ testing/tests/lorem/test.sh
@@ -4,7 +4,7 @@ before_test() {
     tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter ":redir! > $log_file" enter
 }
 test_func() {
-    tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter "ggdGitest start" enter "lorem" escape "a" tab "otest endQq"
+    tmux send-keys -t $tmux_session "ggdGitest start" enter "lorem" escape "a" tab "otest endQq"
 }
 after_test() {
     tmux send-keys -t $tmux_session ":redir END" enter ":x" enter
