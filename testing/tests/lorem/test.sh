04bbdf460a895529fd233246b909a1ffa27760b3
Andrey Orst <andreyorst@gmail.com>
2018-05-24 14:51:09 +0300

    another way to store message log

--- testing/tests/lorem/test.sh
+++ testing/tests/lorem/test.sh
@@ -1,11 +1,11 @@
 test_file=lorem
 before_test() {
     touch $test_file
-    tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter ":redir => log" enter
+    tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter ":redir! > $log_file" enter
 }
 test_func() {
     tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter "ggdGitest start" enter "lorem" escape "a" tab "otest endQq"
 }
 after_test() {
-    tmux send-keys -t $tmux_session ":redir END" enter ":let @\" = log" enter "p:x" enter
+    tmux send-keys -t $tmux_session ":redir END" enter ":x" enter
 }
--- testing/tests/shell/test.sh
+++ testing/tests/shell/test.sh
@@ -1,11 +1,11 @@
 test_file=shell.c
 before_test() {
     touch $test_file
-    tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter ":redir => log" enter
+    tmux send-keys -t $tmux_session "$vim -n -u ../../testrc $test_file" enter ":redir! > $log_file" enter
 }
 test_func() {
     tmux send-keys -t $tmux_session "ggdGitest start" enter "echo" escape "a" tab tab enter "test endQq"
 }
 after_test() {
-    tmux send-keys -t $tmux_session ":redir END" enter ":let @\" = log" enter "p:x" enter
+    tmux send-keys -t $tmux_session ":redir END" enter ":x" enter
 }
